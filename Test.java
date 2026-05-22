f468d813c389a39418420bd1a623d41bb7ae6306
cser.tanishq.verma <tanishqv29@icloud.com>
2026-05-22 14:33:56 +0530

    1 commit

--- Test.java
+++ Test.java
@@ -2,8 +2,8 @@ public class Test {
 
   static void add(){
 
-    int a=37;
-    int b=37;
+    int a=38;
+    int b=38;
     int result = a*b;
     System.out.println("Hello Ji");
     System.out.println("The sum is " +result);
